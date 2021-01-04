610d01e0927fd3c280f87d572a9d3d37541a8c04
Cesar Celis <celis212@gmail.com>
2021-01-04 17:55:48 -0500

    homework done

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ var text = "";
 
 while (count < 10) {
     count++;
-    text
+    alert("the number using while is " + count)
+}
+
+var count_two = null;
+
+for (count_two = 1; count_two <= 10; count_two++) {
+    alert("the number using for is " + count_two)
 }
