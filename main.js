2c1ff89e2570fb0c17177701771554974a0182dc
Cesar Celis <celis212@gmail.com>
2021-01-21 18:19:14 -0500

    two

---
+++ main.js
@@ -0,0 +1,16 @@
+// la funciones siempre llevan un return pero si no lo pone lo lleva implicito y queda como indefinido
+function double(a) {
+    return a * 2
+}
+
+//arrow function
+// se le puede eliminar la palabra function
+const doubleTwo (a) => {
+    return a * 2
+}
+
+// se le puede quitar los corchetes y la palabra return, pero sola funciona cuando la funcion tiene una sola linea de codigo
+const doubleThree = (a) => a * 2
+
+//suprime los parentesis
+const doubleFour = a => a * 2
