485b599d1af5ab879b7a647ea73ad246531ec5b8
Cesar Celis <celis212@gmail.com>
2021-01-27 13:04:04 -0500

    MIDDLE POINT

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//Agrega un nuevo ítem al menú de navegación desde Javascript.
+
+// console.log("hello world")
+// let firstT = document.createElement('li');
+// firstT.textContent = 'Item 5';
+
+// let addNavbar = document.querySelector('.nav-list')
+// addNavbar.appendChild(firstT)
+
+
+//Agrega una negrita a una sola palabra dentro de tu párrafo.
+
+// let nodeElement = document.querySelector('.text p')
+// console.log(nodeElement)
+// let textToArray = Array.from(nodeElement)
+// console.log(textToArray)
