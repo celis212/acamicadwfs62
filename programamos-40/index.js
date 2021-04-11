3b40b4abdf7101a7113f5f30f96e2aa77fa3cbb1
Cesar Celis <celis212@gmail.com>
2021-04-11 16:49:30 -0500

    done

--- programamos-40/index.js
+++ programamos-40/index.js
@@ -5,7 +5,12 @@ var moment = require('moment');
 
 let local = moment(); 
 let utc = moment().utc();
+let diferencia = moment(utc.diff(local));
+let colombia = utc.add(-5, 'hours');
+let argentina = utc.add(-3, 'hours')
 
 console.log(local);
 console.log(utc);
 console.log(local.diff(utc, 'hours'));
+console.log(colombia);
+console.log(argentina); 
